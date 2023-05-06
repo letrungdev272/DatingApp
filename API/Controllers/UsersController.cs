1382e233191c98d0576f4a2e3a2f0041bfe1ef02
LeTrung272 <thanhtrung272t@gmail.com>
2023-05-06 22:24:48 +0700

    End of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UsersController : ControllerBase
+	{
+		private readonly DataContext _context;
+		public UsersController(DataContext context)
+		{
+			_context = context;
+		}
+		
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+		{
+			return await _context.User.ToListAsync();
+		}
+		
+		[HttpGet("{id}")]
+		public async Task<ActionResult<AppUser>> GetUser(int id)
+		{
+			return await _context.User.FindAsync(id);
+		}
+	}
+}
---
+++ API/Program.cs
@@ -0,0 +1,21 @@
+using System.Collections.Immutable;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+	opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
